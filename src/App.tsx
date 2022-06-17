4dedf67f5e145d2091c944d7a0fbc6a3f85dee7f
samantha gadet <samantha@merinio.com>
2022-06-17 11:45:39 -0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { AppBar, ThemeProvider, Toolbar, Typography } from '@material-ui/core'
+import { theme } from './theme'
+
+export const App: React.FunctionComponent = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography>Technical Assignment</Typography>
+          </Toolbar>
+        </AppBar>
+        {/* TODO: Add components here */}
+      </div>
+    </ThemeProvider>
+  )
+}
